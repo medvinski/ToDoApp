297629e741ec63e9e4eb17ac42149c54315e00b5
medvinski <103368186+medvinski@users.noreply.github.com>
2023-06-04 06:39:08 +0200

    first commit

---
+++ react-todo/src/Todo/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {HomeStyle} from "./Homepage.style";
+
+const Homepage = () => {
+  return (
+    <div className={HomeStyle.todoContainer}> This is homepage
+      
+    </div>
+  );
+};
+
+export default Homepage;
